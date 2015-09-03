f4def73508fd2e72074cfdd9653a9324c498735b
Japh <japhie@gmail.com>
2015-09-03 21:26:32 +1000

    Remove superfluous request method

--- inc/class-stream-papertrail-api.php
+++ inc/class-stream-papertrail-api.php
@@ -23,34 +23,7 @@ class Stream_Papertrail_API {
 			$record['meta']['user_meta'] = unserialize( $record['meta']['user_meta'] );
 		}
 
-		$record = json_encode( $record );
-
-		$this->request( $record );
-
-	}
-
-	public function request( $args = array() ) {
-
-		$url = STREAM_LOGGLY_URL;
-
-		$this->send_remote_syslog( $args );
-
-		$request = wp_remote_request( $url, array(
-			'method' => 'POST',
-			'headers' => array(
-				'Content-Type' => 'text/plain'
-			),
-			'body' => $args,
-			'blocking' => false
-		));
-
-		if ( is_wp_error( $request ) ) {
-			return $request;
-		}
-
-		$response = json_decode( wp_remote_retrieve_body( $request ) );
-
-		return $response;
+		$this->send_remote_syslog( json_encode( $record ) );
 
 	}
 
